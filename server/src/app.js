8ebbddef6112f0ce19dfc077cc3c062115b761d8
arsh-deep-0 <96560863+arsh-deep-0@users.noreply.github.com>
2024-04-14 02:35:38 +0530

    set trust proxy to 1 in backnd

--- server/src/app.js
+++ server/src/app.js
@@ -27,6 +27,8 @@ app.use(
 app.set("io", io);
 initializeSocketIO(io);
 
+app.set('trust proxy', 1);
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
